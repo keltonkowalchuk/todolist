f4e2424a12b29f027464acc82b3473da4ea84dd5
Kelton Kowalchuk <keltonkowalchuk@gmail.com>
2014-02-26 18:41:58 -0800

    Changed to a more MVC structure

--- assets/js/toDo.js
+++ assets/js/toDo.js
@@ -1,65 +1,57 @@
-var activeTodos = [];
-var completeTodos = [];
+function toDoItem(id,text) {
+	this.id = id;
+	this.text = text;
+	this.isComplete = false;
+};
 
-var toDo = {
+var toDoController = {
 	
 	totalTodos: 0,
-
-	create : function(input) {
-		for(key in this){
-			if(this[key].input == input){
-				alert("This ToDo matches another ToDo that is already on your list, please enter a different ToDo");
-				return false;
-			} else if(input.length === 0 || jQuery.trim(input).length === 0) {
-				alert("This ToDo is empty, please enter a valid ToDo");
-				return false;
-			}
-		}
+	toDoList: [],
+  
+	createToDoItem : function(text) {
+		var toDoItem = new toDoItem(this.totalTodos,text);
+		this.toDoList.push(toDoItem);
 		this.totalTodos++;
-		this["todo" + this.totalTodos] = {
-			isComplete: false,
-			input: input,
-			id: this.totalTodos
-		}
-		activeTodos.push(this["todo" + this.totalTodos]);
-		this.updateList(this["todo" + this.totalTodos]);
+		this.updateListView();
 	},
-
-	delete : function(id) {
-		for( key in this ){
-			if( this[key].id == id ){
-				this[key].isComplete = true;
-				if(completeTodos.length >= 0 ){
-					completeTodos.push(this[key]);
-					var index = activeTodos.indexOf(this[key]);
-					activeTodos.splice(index, 1);
-			  }
+	
+	completeToDoItem : function(id) {
+		for(i in this.toDoList) {
+			if(this.toDoList[i].id == id) {
+				this.toDoList[i].isComplete = true; 
 			}
 		}
+		toDoController.updateListView();
 	},
-
-	updateList : function(todoName) {
-			 var div = document.createElement('div');
-			 div.className = 'checkbox';
-			 div.innerHTML = '<label> <input type="checkbox" id="'+ todoName["id"] +'" value="">' + todoName["input"] + '</label>';
-			 document.getElementById('checkList').appendChild(div);
-		},
+  
+	updateListView : function() {
+		document.getElementById('checkList').innerHTML = '';
+		for(i in this.toDoList) {
+			if(!this.toDoList[i].isComplete) {
+				var div = document.createElement('div');
+				div.className = 'checkbox';
+				div.innerHTML = '<label> <input type="checkbox" id="'+ this.toDoList[i].id +'" value="">' + this.toDoList[i].text + '</label>';
+				document.getElementById('checkList').appendChild(div);
+			}
+		}	
+	},
 
 	init : {
 
-		create : function() {
-			$('.inputToDo').keyup(function(e){
+		createDisplay : function() {
+			$('.inputToDo').on("keyup", function(e){
 				if(e.keyCode == 13){
-					var data = $('.inputToDo').val();
-					toDo.create(data);
+					var text = $('.inputToDo').val();
+					toDoController.createToDoItem(text);
 					$('.inputToDo').val('');
 				}
 			});
 		},
 
-		delete : function() {
+		deleteDisplay : function() {
 			$(document).on("click", ".checkbox input", function(){
-				toDo.delete($(this).attr("id"));
+				toDoController.completeToDoItem($(this).attr("id"));
 				$(this).parent().parent().hide();
 			})
 		}
@@ -68,6 +60,6 @@ var toDo = {
 };
 
 $(document).ready(function(){
-	toDo.init.create();
-	toDo.init.delete();
+	toDoController.init.createDisplay();
+	toDoController.init.deleteDisplay();
 });
