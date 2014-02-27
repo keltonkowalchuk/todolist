92605fe629361de3ba3a421cb651521d1942cff5
Kelton Kowalchuk <keltonkowalchuk@gmail.com>
2014-02-27 06:31:30 -0800

    Changed array to hashmap, commented code

--- assets/js/toDo.js
+++ assets/js/toDo.js
@@ -5,15 +5,16 @@ function toDoItem(id,text) {
 };
 
 var toDoController = {
-	
+	// Global variables
 	totalTodos: 0,
-	toDoList: [],
+	toDoList: {},
 
 	init : function() {
 		toDoController.bindActions();
 	},
 
 	bindActions : function() {
+		// On enter, create todo with text
 		$('.inputToDo').on("keyup", function(e){
 			if(e.keyCode == 13){
 				var text = $('.inputToDo').val();
@@ -21,31 +22,33 @@ var toDoController = {
 				$('.inputToDo').val('');
 			}
 		});
-
+		// On click of checkbox, mark item as completed
 		$(document).on("click", ".checkbox input", function(){
 			toDoController.completeToDoItem($(this).attr("id"));
 		});
 	},
   
 	createToDoItem : function(text) {
-		var newToDoItem = new toDoItem(this.totalTodos,text);
-		this.toDoList.push(newToDoItem);
-		this.totalTodos++;
+		// Create new toDoItem with inputted text
+		var newToDoItem = new toDoItem(this.totalTodos++,text);
+		// Add toDoItem to hashmap
+		this.toDoList[newToDoItem.id] = newToDoItem;
+		// Update HTML
 		this.updateView();
 	},
 	
 	completeToDoItem : function(id) {
-		for(i in this.toDoList) {
-			if(this.toDoList[i].id == id) {
-				this.toDoList[i].isComplete = true; 
-			}
-		}
+		// Set toDo matching ID to complete
+		this.toDoList[id].isComplete = true;
+		// Update HTML
 		this.updateView();
 	},
   
 	updateView : function() {
+		// Erase HTML
 		document.getElementById('checkList').innerHTML = '';
-		for(i in this.toDoList) {
+		// Loop through list and display noncomplete toDos
+		for(var i in this.toDoList) {
 			if(!this.toDoList[i].isComplete) {
 				var div = document.createElement('div');
 				div.className = 'checkbox';
